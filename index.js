4dd232830f2bd1f9a55fded4e1bca35a27b2a6c8
robBowes <32183505+robBowes@users.noreply.github.com>
2018-06-21 13:30:14 -0400

    convert project to typescript

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+"use strict";
+/**
+ * Returns true if the value is anything but null
+ *
+ * @param {*} value
+ * @returns {bool} existy
+ */
 const existy = (x) => x != null;
-
 module.exports = {
     existy
 };
---
+++ index.ts
@@ -0,0 +1,12 @@
+
+
+/**
+ * Returns true if the value is anything but null
+ *
+ * @param {*} value
+ */
+const existy = (x: any) : boolean => x != null;
+
+export = {
+    existy
+};
